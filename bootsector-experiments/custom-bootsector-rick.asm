d2e8c9f72d24877b3b9d8a6ff432afd09ad65854
Rick Companje <rick@companje.nl>
2022-04-27 23:37:19 +0200

    using horizontal flip takes to many bytes of code. may be optimised but doubts

--- bootsector-experiments/custom-bootsector-rick.asm
+++ bootsector-experiments/custom-bootsector-rick.asm
@@ -2,6 +2,9 @@
 cpu 8086
 org 0x0
 
+RED equ 0xf000
+GREEN equ 0x0c00
+BLUE equ 0xf400
 cols equ 80
 rows equ 25
 lines equ 200
@@ -11,7 +14,7 @@ setup:
 
 draw:
     cld
-    mov bp,0xf000   ;red
+    mov bp,RED
     mov es,bp
     xor di,di
 
@@ -42,77 +45,90 @@ x:
     xor si,si ; start at 1st col
     mov di,4  ; start at 2nd col
 
-    mov bp,0xf000  ;source ds:si (red)
+    mov bp,RED  ;source ds:si (red)
     mov ds,bp
-    mov bp,0x0c00  ;dest es:di (green)
+    mov bp,GREEN  ;dest es:di (green)
     mov es,bp
-    
-    mov cx,16*2*4
 
-hcopy:
+    mov cx,16                ; 16 cols    
+mirror_copy_outer_loop:
+    push cx
+    mov cx,16                ; 16 cols
 
-    ; push cx
-    ; mov cx,4
+mirror_copy_inner_loop:
+; hcopy:
+    push cx
+    mov cx,4                 ; 4 lines per row
 lines_in_row:
-    lodsb
+    lodsb                    ; al=[ds:si++] (vram)
 
-    push cx    ; somehow repne scasb destroys cx
+    push cx                  ; somehow repne scasb destroys cx
     push es
     push di
 
-    push cs   ; es=cs
-    pop es    ; es=cs, needed for scasb, somehow cannot use inline cs
+    push cs                  ; es=cs
+    pop es                   ; es=cs, needed for scasb, somehow cannot use inline cs with repne/scasb
 
+    mov cx,8                 ; repne uses cx
     mov di,lut
-    repne scasb ; cmp al,es:di, inc di
-    cs mov al,[di+8] ; mirror using lut
+    repne scasb              ; cmp al,[es:di++] (lut)
+    cs mov al,[di+8]         ; mirror using lut
 
     pop di
     pop es
     pop cx
     
-    stosb
+    stosb                    ; [es:di++]=al (vram)
 
-    ; loop lines_in_row
-    ; pop cx
-    ; add si,4 ;skip 1 column
-    ; add di,4 ;skip 1 column
+    loop lines_in_row
 
-    loop hcopy
+    pop cx
+    add si,4                 ; skip 1 column
+    add di,4                 ; skip 1 column
+
+    loop mirror_copy_inner_loop
+    pop cx
+
+    add si,512                ; 192 + 320; 192=remaining space, 320=whole row
+    add di,512                ; 192 + 320; 192=remaining space, 320=whole row
+
+    loop mirror_copy_outer_loop
+
+;final vertical copy loop
 
     hlt
 
 cls:
-    mov ax,0x0c00
+    mov ax,GREEN
     mov cx,0x4000
     xor di,di
-    mov es,ax    ; green
-    rep stosb
-    mov ah,0xf0
-    mov es,ax    ;  red + blue 
+    mov es,ax                            ; green
+    rep stosb                        
+    mov ah,0xf0                          ; ax=RED
+    mov es,ax                            ; red + blue 
     xor di,di
     mov ch,0x80
     rep stosb
     ret
     
-img: 
-    db 0, 0, 0, 0
-    db 0, 0, 0, 1         ;punt
-    db 0, 0, 0, 3         ;min
-    db 0, 0, 1, 3         ;plus
-    db 0, 0, 3, 7         ;hoed
-    db 0, 0, 7, 7         ;blok
-    db 0, 1, 7, 15        ;ster
-    db 0, 3, 15, 15       ;dikke plus
-    db 0, 15, 31, 63   
-    db 1, 31, 63, 63      ;tol
-    db 7, 31, 31, 63      ;robothoofd
+img:                                     ; 60 bytes
+    db 0, 0, 0, 0                        ; empty
+    db 0, 0, 0, 1                        ; dot
+    db 0, 0, 0, 3                        ; minus
+    db 0, 0, 1, 3                        ; plus
+    db 0, 0, 3, 7                        ; hat
+    db 0, 0, 7, 7                        ; block
+    db 0, 1, 7, 15                       ; star
+    db 0, 3, 15, 15                      ; fat plus
+    db 0, 15, 31, 63                   
+    db 1, 31, 63, 63                     ; tol
+    db 7, 31, 31, 63                     ; robot head
     db 15, 31, 63, 63
     db 15, 63, 63, 127
     db 31, 63, 127, 127
     db 31, 127, 255, 255
     db 63, 127, 255, 255
 
-lut:
-    db 0,1,3,7,15,31,63,127,255
-    db 0,0,128,192,224,240,248,252,254
+lut:                                     ; 18 bytes
+    db 0,1,3,7,15,31,63,127,255          ; key
+    db 0,0,128,192,224,240,248,252,254   ; value
