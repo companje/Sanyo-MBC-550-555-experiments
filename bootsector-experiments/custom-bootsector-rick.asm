470eabe71d8ea2c91fa41cca2556a801c08af177
Rick Companje <rick@companje.nl>
2022-05-03 01:31:30 +0200

    red and white

--- bootsector-experiments/custom-bootsector-rick.asm
+++ bootsector-experiments/custom-bootsector-rick.asm
@@ -7,7 +7,6 @@ setup:
     call render_chars
 
     mov di,0
-
     mov cx,15
 y:
     push cx
@@ -15,14 +14,21 @@ y:
 x:
     mov ax,cx
 
-    ; mov bp,draw_char
-    
+    sub al,7
+
+    cmp al,0
+    pushf
+    jge positive
+    neg al
+positive:
     mov bp,RED
     call draw_char
-
+    popf
+    jge green_blue
+    xor al,al ; if negative then color=red, so clear green and blue
+green_blue:
     mov bp,GREEN
     call draw_char
-
     mov bp,BLUE
     call draw_char
 
@@ -83,4 +89,3 @@ cls:
 %warning total num
 
 data:
-    times 2000 db 0
